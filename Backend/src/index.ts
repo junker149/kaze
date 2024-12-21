b44b3cba2f17725857eab8941b3360ab7f83bfa0
junker149 <rawalaman.0505@gmail.com>
2024-12-21 15:26:45 +0530

    Add POST:api/v1/blog

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -6,25 +6,34 @@ import { sign, verify } from 'hono/jwt'
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string,
-    JWT: any,
     token: string,
+    JWT: string,
     jwt: string
+  },
+  Variables: {
+    userId: string
   }
 }>()
 
+
 app.use('/api/v1/blog/*', async (c, next) => {
   const jwt = c.req.header("authorization");
-  const token = jwt?.split('')[1];
+  const token = jwt?.split(' ')[1];
+  console.log(token);
   // @ts-ignore
-  const payload = await verify({token: token}, c.env.JWT);
-  if (payload.id) {
-    next();
+  const payload = await verify(token, c.env.JWT);
+  if (!payload.id) {
+    c.status(403);
+    return c.json({
+      message: 'Forbidden'
+    })
   }
-  c.status(403);
-  return c.json({
-    message: 'Forbidden'
-  })
+  console.log(payload.id);
+  // @ts-ignore
+  c.set("userId", payload.id);
+  await next();
 })
+
 
 app.get('/', (c) => {
   return c.text('Hello Kaze!')
@@ -66,38 +75,61 @@ app.post('/api/v1/user/signin', async (c) => {
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
 
-  try{
+  try {
     const body = await c.req.json();
     const user = await Client.user.findUnique({
       where: { email: body.email }
     })
 
     // User not found
-    if(!user){
+    if (!user) {
       c.status(403);
       return c.json({
         message: 'Forbidden'
       });
     }
 
-    const token = await sign({id: user.id}, c.env.JWT);
+    const token = await sign({ id: user.id }, c.env.JWT);
 
     c.status(200);
     return c.json({
       message: 'OK',
       token: token
     })
-  }catch(err){
+  } catch (err) {
     console.log(err);
     c.status(500);
-    c.json({
+    return c.json({
       message: "Internal Server Error"
     })
   }
 })
 
-app.post('/api/v1/blog', (c) => {
-  return c.text('Hello Kaze! Post Blog')
+app.post('/api/v1/blog', async (c) => {
+  const Client = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  const body = await c.req.json();
+  const userId = c.get('userId');
+  try{
+    const post = await Client.post.create({
+      data: {
+        title: body.title,
+        content: body.content,
+        authorID: userId
+      }
+    })
+    c.status(201);
+    return c.json({
+      message: 'Created',
+    })
+  }catch(err){
+    console.log(err);
+    c.status(500);
+    return c.json({
+      message: 'Internal Server Error'
+    })
+  }
 })
 
 app.put('/api/v1/blog', (c) => {
