a7e203c482729b14480c742d5dbc05d01d92c0e4
junker149 <rawalaman.0505@gmail.com>
2025-01-26 11:04:08 +0530

    Debugging

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,5 +1,19 @@
 import zod from 'zod';
 
+export const signupInput = zod.object({
+    name: zod.string().min(2),
+    email: zod.string().email(),
+    password: zod.string().min(8)
+});
+
+export const signinInput = zod.object({
+    email: zod.string().email(),
+    password: zod.string().min(8)
+})
+
+export type SignupInput = zod.infer<typeof signupInput>;
+export type SigninInput = zod.infer<typeof signinInput>;
+
 export const postBlogInput = zod.object({
     title: zod.string().min(2),
     content: zod.string().min(2),
