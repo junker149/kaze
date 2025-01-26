5b55ac416729c2818e09ba60115fe80dfd2844a0
junker149 <rawalaman.0505@gmail.com>
2025-01-26 17:36:42 +0530

    Initializing frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='text-6xl flex items-center h-screen justify-center'>KAZE</div>
+    </>
+  )
+}
+
+export default App
