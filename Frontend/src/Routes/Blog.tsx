b2438c4df8bacfae3bcd17fb74678ef8164b4673
junker149 <rawalaman.0505@gmail.com>
2025-02-07 22:16:51 +0530

    feat: Complete Blog page CSS

--- Frontend/src/Routes/Blog.tsx
+++ Frontend/src/Routes/Blog.tsx
@@ -5,20 +5,23 @@
 // }
 // {title, metadata, content}: InputParams
 export default function Blog() {
-    return <div className="flex h-screen p-3">
-        <div className=" w-7/10 text-left p-25 pr-4 border-1 border-gray-200 border-r-0">
-            {/* <div>{title}</div>
+    return <div className="h-screen">
+        <div className="m-3 flex border-1 border-gray-200">
+            <div className=" w-7/10 text-left p-25 pr-4 border-1 border-gray-200 border-r-0">
+                {/* <div>{title}</div>
             <div>{metadata}</div>
             <div>{content}</div> */}
-            <div className="text-6xl font-extrabold mb-2 mt-15">Taxing Laughter: The Joke Tax Chronicles</div>
-            <div className="text-gray-300">Posted on August 24, 2023</div>
-            <div className="mt-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, impedit autem repudiandae suscipit fugiat aliquam eligendi nisi reprehenderit molestiae iure quas delectus. Saepe vitae earum repellendus cum nulla.Voluptatum, nulla.</div>
-        </div>
-        <div className="w-3/10 text-left p-25 pl-10 border-1 border-gray-200 border-l-0">
-            <div className="mt-12 font-semibold text-lg text-gray-400">Author</div>
-            <div className="flex bg-red-100">
-                <div></div>
-                <div></div>
+                <div className="text-6xl font-extrabold mb-2 mt-15">Taxing Laughter: The Joke Tax Chronicles</div>
+                <div className="text-gray-300">Posted on August 24, 2023</div>
+                <div className="mt-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam praesentium eligendi ex quod suscipit corporis architecto illum tenetur unde, quo iusto id fuga enim odit omnis? Odit provident voluptate veniam reiciendis quasi dolor praesentium quos reprehenderit sit amet laborum, sunt debitis! Error expedita sed sint nisi quaerat. Necessitatibus, consequatur. Asperiores nesciunt fuga iste vitae praesentium deserunt laboriosam, consectetur ipsa, est cum dolores nobis impedit hic, tempore officia ab ipsum temporibus! Enim vel accusantium dignissimos velit debitis provident repellendus placeat commodi dicta itaque earum eaque quisquam, explicabo porro consequuntur praesentium aliquam laudantium qui cumque iusto nobis pariatur eveniet? Alias natus officiis similique possimus saepe dolore eligendi odit magnam aliquid necessitatibus inventore dignissimos harum illo fugit et quam exercitationem, esse molestias tempora itaque assumenda? Ab itaque repellendus quisquam ratione maxime dolor nemo saepe, iure atque quaerat. Quia enim explicabo labore ea nulla! Quidem neque laborum, eum adipisci odio ad! Necessitatibus, ea eum et ab ex amet voluptate numquam sint. Exercitationem nemo soluta odit repellendus quidem, maxime atque consectetur eum facere possimus aspernatur numquam impedit architecto temporibus adipisci molestiae consequatur dolorum libero quia eligendi aliquid quis repellat? Perspiciatis eos quis culpa aperiam tempora delectus dolore odit laborum est obcaecati, officiis iure architecto corporis dolorem qui numquam reprehenderit vel aliquam accusamus consectetur! Cupiditate facilis dolor assumenda accusantium libero ipsa, esse facere temporibus repellat illum suscipit consectetur provident, saepe explicabo consequuntur perferendis, beatae tempora nesciunt adipisci nam accusamus enim. Excepturi, architecto reprehenderit culpa dolore facilis soluta error ratione minima ducimus commodi similique quae veritatis ipsam at deserunt quia. Libero in nesciunt molestias necessitatibus. Magni quibusdam iure eveniet debitis quis ratione voluptate quam hic totam sit fugit deleniti unde, error voluptas porro? Vero, nemo nesciunt ducimus, quasi quia, corrupti optio totam autem sunt voluptates dolores? Sequi libero asperiores beatae non, sint perspiciatis eius vitae sapiente error vel eaque tempore quam ad ex iusto delectus dolore sunt natus ullam perferendis atque? Dolorum, aperiam soluta adipisci praesentium, delectus pariatur tempore reprehenderit eligendi ducimus ipsum numquam similique tempora minima quos voluptas. Eligendi, illum provident. Quidem, repellendus. Nostrum, temporibus quidem! Dignissimos quos dolorem aliquam maxime ab ad blanditiis, dolorum voluptates laudantium similique numquam reiciendis fuga error quaerat veritatis totam fugit accusantium rem deleniti asperiores iusto assumenda libero. Eum dolorum facere in, quod earum tenetur voluptas nisi aliquam, doloremque possimus vero et. Unde saepe itaque officia nostrum temporibus beatae distinctio rem. Exercitationem, quam obcaecati minima aut earum corrupti autem? Praesentium, sed.</div>
+            </div>
+            <div className="w-3/10 text-left p-25 pl-10 border-1 border-gray-200 border-l-0">
+                <div className="mt-12 font-semibold text-lg text-gray-400">Author</div>
+                <div className="flex mt-4">
+                    <div className="w-10 h-2 bg-gray-200 rounded-2xl mt-2"></div>
+                    {/* <div>{flair}</div> */}
+                    <div className="ml-5 text-gray-800 font-bold text-xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, quas?</div>
+                </div>
             </div>
         </div>
     </div>
